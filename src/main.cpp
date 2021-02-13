a259adac2d3c6d6f4648659fa64868559439a350
olwmc <olwmc@protonmail.ch>
2021-02-13 03:02:46 -0500

    Added: ReturnStatement test

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,20 @@
 int main() {
     ASTVisitor vis("Context-Goes-Here");
 
-    /* String literal */
-    StringLiteral str_1("My string!!");
+    // 1.253, 3.687
+    NumberLiteral num_1(1.253);
+    NumberLiteral num_2(3.687);
 
-    /* 20 - 13 + 8 */
-    NumberLiteral num_1 (20);
-    NumberLiteral num_2 (13);
-    NumberLiteral num_3 (8);
+    // 1.253 * 3.687
+    BinaryExpression expr_1(&num_1, &num_2, Operator::MULTIPLY);
 
-    /* 20 - 13 */
-    BinaryExpression expr_1(&num_1, &num_2, Operator::SUBTRACT);
-
-    /* (20 - 13) + 8 */
-    BinaryExpression expr_2(&expr_1, &num_3, Operator::ADD);
-
-    /* ((20 - 13) + 8) * 20 */
-    BinaryExpression expr_3(&expr_2, &num_1, Operator::MULTIPLY);
+    // return (1.253 * 3.687)
+    ReturnStatement ret_1(&expr_1);
     
-    NumberLiteral num_4 (300);
-    NumberLiteral num_5 (0);
-    
-    /* (((20 - 13) + 8) * 20) == 300 */
-    BinaryExpression expr_4(&expr_3, &num_4, Operator::NOTEQUAL);
+    std::vector<ASTNode*> body;
+    body.push_back(&ret_1);
 
-    /* ((((20 - 13) + 8) * 20) == 300) && 0 */
-    BinaryExpression expr_5(&expr_4, &num_5, Operator::AND);
-    
-    std::cout << "Expr_3: " << expr_3.accept(vis).asNumber() << "\n";
-    std::cout << expr_5.accept(vis).asNumber() << "\n";
+    Block block_1(body);
+
+    std::cout << block_1.accept(vis).asNumber() << "\n";
 }
