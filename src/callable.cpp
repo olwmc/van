d663f80de071da4859b6574e1949bf6e877511c6
olwmc <olwmc@protonmail.ch>
2021-02-15 11:47:00 -0500

    Added: Comments for builtin print

--- src/callable.cpp
+++ src/callable.cpp
@@ -6,10 +6,20 @@
 
 #include <iostream>
 
+Value ASTVisitor::visit(UserFunction& userFunction) {
+  return Value();
+}
+
+
 Value ASTVisitor::visit(builtin_Print& printStatment) {
+  // Get the input from context
+  // Using "printStatement.args()" instead of __input__ to make this more
+  // maintainable
   Value input = this->m_context->resolveVariable(printStatment.args()[0]);
 
+  // Print the input
   std::cout << input.toString() << "\n";
 
+  // Return NIL
   return Value();
 }
