d527cc8aac7742d8a1c3dc7cd4b61e6b2e96f86d
olwmc <olwmc@protonmail.ch>
2021-02-15 13:49:21 -0500

    Added: comments

--- src/callable.cpp
+++ src/callable.cpp
@@ -7,12 +7,14 @@
 
 #include <iostream>
 
+/* User defined function */
 Value ASTVisitor::visit(UserFunction& userFunction) {
   Value v = userFunction.body()->accept(*this);
   return v;
 }
 
 
+/* Builtin print statement */
 Value ASTVisitor::visit(builtin_Print& printStatment) {
   // Get the input from context
   // Using "printStatement.args()" instead of __input__ to make this more
@@ -26,6 +28,7 @@ Value ASTVisitor::visit(builtin_Print& printStatment) {
   return Value();
 }
 
+/* Builtin assert statement */
 Value ASTVisitor::visit(builtin_Assert& assertStatement) {
   // Assert that the value is true
   Value condition = this->m_context->resolveVariable(assertStatement.args()[0]);
