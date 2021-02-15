06b91108812b2eac8a4425460a1da5c397206aaa
olwmc <olwmc@protonmail.ch>
2021-02-14 22:41:51 -0500

    Added: toString method

--- src/value.cpp
+++ src/value.cpp
@@ -1,2 +1,30 @@
 #include "value.h"
-// std::string Value::toString() {}
+#include <string>
+
+std::string Value::toString() {
+    if(this->m_type == Value_Type::NUMBER) {
+        return std::to_string(this->m_number);
+    }
+
+    else if (this->m_type == Value_Type::STRING) {
+        return this->m_string;
+    }
+
+    return "NIL";
+}
+
+/* Arithmetic operations */
+// Value operator+(const Value& other);
+// Value operator-(const Value& other);
+// Value operator*(const Value& other);
+// Value operator%(const Value& other);
+
+// /* Comparators */
+// Value operator==(const Value& other);
+// Value operator!=(const Value& other);
+// Value operator<=(const Value& other);
+// Value operator>=(const Value& other);
+    
+// /* Logical Operations */
+// Value operator&&(const Value& other);
+// Value operator||(const Value& other);
