5d49cfaad0302e8fba95e2446ee3c1602e2586c2
olwmc <olwmc@protonmail.ch>
2021-02-15 20:50:52 -0500

    Added: Forloop test

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,27 @@ int main() {
 
     // Give the scope to the visitor
     ProgramVisitor vis(&context);
+    
+    // init
+    NumberLiteral num_0(0);
+    VariableDeclaration init_1("myVar", &num_0, false);
 
-    StringLiteral str_1("Hello!!!!!");
-    FunctionCall fc_1("print", {&str_1});
+    // Update
+    Identifier id_1("myVar");
+    NumberLiteral num_1(1);
+    BinaryExpression bin_1(&id_1, &num_1, Operator::ADD);
+    AssignmentStatement update_1("myVar", &bin_1);
 
-    fc_1.accept(vis);
+    // Test
+    NumberLiteral num_2(10);
+    BinaryExpression test(&id_1, &num_2, Operator::LESSEQUAL);
+
+    // Block
+    FunctionCall print_call("print", { &id_1 });
+    Block block({&print_call});
+
+    ForLoop loop_1(&init_1, &update_1, &test, &block);
+
+    loop_1.accept(vis);
+    context.popScope();
 }
