4f7487a55c5ed91ffb64d8729678b9796e8a519c
olwmc <olwmc@protonmail.ch>
2021-02-18 21:52:39 -0500

    Changed: main test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
+#include "AST.h"
 #include "context.h"
+#include "lexer.h"
+
+#include <fstream>
+#include <iostream>
 
 int main() {
+    std::ifstream infile("examples/fib.van");
+    std::string file_contents { std::istreambuf_iterator<char>(infile), std::istreambuf_iterator<char>() };
 
     // Create a new context and push a local scope
     Context context;
@@ -11,13 +18,13 @@ int main() {
     
     // init
     NumberLiteral num_0(0);
-    VariableDeclaration init_1("myVar", &num_0, true);
+    VariableDeclaration init_1("i", &num_0, true);
 
     // Update
-    Identifier id_1("myVar");
+    Identifier id_1("i");
     NumberLiteral num_1(1);
     BinaryExpression bin_1(&id_1, &num_1, Operator::ADD);
-    AssignmentStatement update_1("myVar", &bin_1);
+    AssignmentStatement update_1("i", &bin_1);
 
     // Test
     NumberLiteral num_2(10);
@@ -27,8 +34,18 @@ int main() {
     FunctionCall print_call("print", { &id_1 });
     Block block({&print_call});
 
+    // For loop
     ForLoop loop_1(&init_1, &update_1, &test, &block);
 
-    loop_1.accept(vis);
+    StringLiteral str_1("Goodbye!");
+    FunctionCall pr_1("print", {&str_1});
+
+    StringLiteral hello("Hello!");
+    FunctionCall pr_2("print", {&hello});
+
+    // Outer block
+    Block block_2{ {&pr_2, &loop_1, &pr_1} };
+
+    block_2.accept(vis);
     context.popScope();
 }
