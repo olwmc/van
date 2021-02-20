50af50af44e2d69f1f098bace7e801e00b3b77fc
olwmc <olwmc@protonmail.ch>
2021-02-19 21:42:13 -0500

    Added: Lexer implementation skeleton

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,26 +1,57 @@
 #include "lexer.h"
 
-std::vector<std::string> Token_string = { 
-    "IDEN",
-    "NUM", "STR", "BOOL",
-    "EOF",
-    "OP", "SEP", "PUNC",
+#include <vector>
+#include <string>
+#include <unordered_map>
 
-    "FOR", "RETURN", "DEFUN", "LOCAL", "GLOBAL"
+/* Ordered strings for Token_type */
+std::vector<std::string> token_string = { 
+    "IDENT",
+    "NUM", "STR", "TRUE", "FALSE"
+    "EOF",
+    "OP", "SEP", "PUNCT",
+
+    "FOR", "RETURN", "DEFUN", "LOCAL", "GLOBAL",
+	"AS", "BEGIN", "END",
+    "COMMENT"
 };
 
 std::string Token::toString() {
-    	return "Type: " + Token_string[this->m_type] + "\tLine: " + \
-               std::to_string(this->m_line) + "[" + std::to_string(this->m_index) + "]\tSymbol: \"" + this->m_raw + "\"";
+    	return "Type: " + token_string[this->m_type] + "\tLine: " + \
+               std::to_string(this->m_line) + "{" + std::to_string(this->m_index) + "}" + \
+               "\tSymbol: \"" + this->m_raw + "\"";
 }
 
-Token Lexer::getNextToken() {
-	/* while(testCondition(input[++index])); */
-	
-	if(this->m_index > (int)this->m_prog.length() - 1)  {
-        return Token(Token_type::END_FILE, "EOF", false, this->m_index, this->m_line);
+/* Maps keywords to Token_type enum */
+std::unordered_map<std::string, Token_type> type_map {
+    {"true",   Token_type::TRUE_LIT}, {"false",  Token_type::FALSE_LIT},
+    {"for",    Token_type::FOR},      {"return", Token_type::RETURN},
+    {"defun",  Token_type::DEFUN},    {"local",  Token_type::LOCAL},
+    {"global", Token_type::GLOBAL},   {"as",     Token_type::AS},
+    {"begin",     Token_type::FOR},   {"end",    Token_type::END}
+};
+
+Token_type Lexer::resolveKeyword(std::string raw) {
+    if(type_map.find(raw) != type_map.end()) {
+        return type_map[raw];
     }
 
-
-    return Token(Token_type::END_FILE, "EOF", false, this->m_index, this->m_line);
+    return Token_type::IDENTIFIER;
 }
+
+// /* Get different types of tokens */
+// Token getPunctuation();
+// Token getSeparator();
+// Token getOperator();
+// Token getNumberLiteral();
+// Token getKeywordOrId();
+// Token getComment();
+// Token getStringLiteral();
+
+//  /* Different character catagorizers */
+//  bool isWhiteSpace();
+//  bool isPunctuation();
+//  bool isSeparator();
+//  bool isOperator();
+//  bool isDigit();
+//  bool isAlpha();
