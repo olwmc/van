3983606e2bdd8f3f323a614d20c50e5412bf97cd
olwmc <olwmc@protonmail.ch>
2021-02-19 21:41:20 -0500

    Added: lexer skeleton

--- include/lexer.h
+++ include/lexer.h
@@ -6,13 +6,16 @@
 
 enum Token_type { 
     IDENTIFIER,
-    NUM_LIT, STR_LIT, BOOL_LIT,
+    NUM_LIT, STR_LIT, TRUE_LIT, FALSE_LIT,
     END_FILE,
     OPERATOR, SEPARATOR, PUNCTUATION,
     
-    FOR, RETURN, DEFUN, LOCAL, GLOBAL
+    FOR, RETURN, DEFUN, LOCAL, GLOBAL,
+    AS, BEGIN, END,
+	COMMENT
 };
 
+/* Holds data about each token recovered from the lexer */
 class Token {
 	Token_type m_type;
 	std::string m_raw;
@@ -36,19 +39,42 @@ class Token {
 
 };
 
+/* Gets the next Token in the program string */
 class Lexer {
-	std::string m_prog, currentLexeme;
-	int m_index, m_line;
-	
-	public:
-		Lexer(std::string program)
-			: m_prog(program), m_index(0), m_line(0)
-			{}
+	std::string m_prog;
+    int m_line, m_index;
 
-		/* Emits the next token */
-		Token getNextToken();
-		
-		int line() 			{ return this->m_line;    }
-		int index()			{ return this->m_index;   }
+    /* Index operations */
+	char peek();
+	char peekNext();
+    void advance();
+	
+    // Resolves keyword to corresponding Token_type or identifier
+    Token_type resolveKeyword(std::string raw);
+
+    /* Get different types of tokens */
+    Token getPunctuation();
+    Token getSeparator();
+    Token getOperator();
+    Token getNumberLiteral();
+    Token getKeywordOrId();
+    Token getComment();
+    Token getStringLiteral();
+
+    /* Different character catagorizers */
+    bool isWhiteSpace();
+    bool isPunctuation();
+    bool isSeparator();
+    bool isOperator();
+    bool isDigit();
+    bool isAlpha();
+
+	public:
+		Lexer(std::string program) 
+            : m_prog(program),
+              m_line(0),
+              m_index(0) {}
+
+		Token nextToken();
 };
 #endif /* LEXER_H */
