0156e3657c73c5942758d678eb3429978091ac2e
olwmc <olwmc@protonmail.ch>
2021-04-09 15:49:58 -0400

    Moved: Interpreter to its own class

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,7 @@
-#include "AST.h"
-#include "context.h"
-#include "lexer.h"
-#include "parser.h"
+#include "interpreter.h"
 
 #include <fstream>
 #include <iostream>
-
-class Van_Interpreter {
-    std::string m_program;
-    Context* m_context;
-    Block* m_start;
-
-    public:
-    Van_Interpreter(std::string program) {
-        this->m_program = program;
-        this->m_context = new Context;
-    }
-
-    ~Van_Interpreter() {
-        delete this->m_context;
-        delete this->m_start;
-    }
-
-    Value interpret() {
-        // Lexer and tokens
-        Lexer lexer(this->m_program);
-        std::vector<Token> tokens = lexer.makeTokens();
-
-        // Parsed AST
-        Parser parser(tokens, this->m_program);
-        m_start = parser.parse();
-
-        // Return value
-        Value v;
-
-        // If there were no parser errors, try to run
-        if(!parser.error()) {
-            // Context and visitor
-            this->m_context->pushScope();
-
-            ProgramVisitor vis(this->m_context);
-            
-            // Accept start node (This can throw an exception error)
-            v = m_start->accept(vis);
-        }
-
-        return v;
-    }
-
-    Context* context() {
-        return this->m_context;
-    }
-
-    void setContext(Context* context) {
-        this->m_context = context;
-    }
-};
-
 
 #include <cmath>
 
