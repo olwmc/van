8bcfa994fbf95ce083b2eed1336a4002e69333ce
olwmc <olwmc@protonmail.ch>
2021-04-12 17:34:49 -0400

    Added: Linear regression example program

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 
 int main() {
-    std::ifstream infile("examples/fib.van");
+    std::ifstream infile("examples/regression.van");
     std::string file_contents { std::istreambuf_iterator<char>(infile), std::istreambuf_iterator<char>() };
     
     Van_Interpreter interpreter(file_contents);
