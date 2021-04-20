22aa705b9ac2e369084a200d68711d56b6ba8928
olwmc <olwmc@protonmail.ch>
2021-04-20 16:47:15 -0400

    Added: UpdateIndex

--- src/context.cpp
+++ src/context.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 void Context::bindLocalVariable(std::string name, Value v) {
-    this->m_callStack.back()[name] = v;
+    this->m_scopeStack.back()[name] = v;
 }
 
 void Context::bindGlobalVariable(std::string name, Value v) {
@@ -12,17 +12,17 @@ void Context::bindGlobalVariable(std::string name, Value v) {
 }
 
 void Context::updateVariable(std::string name, Value v) {
-    // Loop through each index of the callstack, looking for the variable
-    for(int i = (int)this->m_callStack.size(); i--;) {
+    // Loop through each index of the scopeStack, looking for the variable
+    for(int i = (int)this->m_scopeStack.size(); i--;) {
 
         // If the variable is found in the map, set the new value and return
-        if(this->m_callStack[i].find(name) != this->m_callStack[i].end()) {
-            this->m_callStack[i][name] = v;
+        if(this->m_scopeStack[i].find(name) != this->m_scopeStack[i].end()) {
+            this->m_scopeStack[i][name] = v;
             return;
         }
     }
 
-    // If the variable isn't in the callstack, check the global scope and set if possible
+    // If the variable isn't in the scopeStack, check the global scope and set if possible
     if(this->m_globalScope.find(name) != this->m_globalScope.end()) {
         this->m_globalScope[name] = v;
         return;
@@ -32,13 +32,66 @@ void Context::updateVariable(std::string name, Value v) {
     throw std::runtime_error("Unable to find variable: " + name);
 }
 
+void Context::updateIndex(std::string name, Value v, int index) {
+    int foundIndex = -1;
+
+    // Loop through each index of the scopeStack, looking for the variable
+    for(int i = (int)this->m_scopeStack.size(); i--;) {
+
+        // If the variable is found in the map, set the new value and return
+        if(this->m_scopeStack[i].find(name) != this->m_scopeStack[i].end()) {
+            foundIndex = i;
+            break;
+        }
+    }
+
+    // If the variable isn't in the scopeStack, check the global scope and set if possible
+    if(this->m_globalScope.find(name) != this->m_globalScope.end()) {
+        foundIndex = -2;
+        return;
+    }
+
+    // Set the new index if it's in the global scope
+    if(foundIndex != -1) {
+        Value_Type t = this->m_scopeStack[foundIndex][name].type();
+
+        // Ensure it's a list otherwhise throw an exception
+        if(t == Value_Type::LIST) {
+            // Get the original list
+            std::vector<Value> alterated = this->m_scopeStack[foundIndex][name].asList();
+
+            // Set the new value at index
+            alterated[index] = v;
+
+            // Set the value to the new list
+            // TODO: NEGATIVE INDEXES
+            this->m_scopeStack[foundIndex][name] = Value(alterated);
+            return;
+
+        } else if (t == Value_Type::STRING) {
+            std::string alterated = this->m_scopeStack[foundIndex][name].asString();
+            alterated[index] = (v.toString())[0];
+
+            this->m_scopeStack[foundIndex][name] = Value(alterated);
+            return;
+        }
+
+        else {
+            throw std::runtime_error("Only lists and strings can be index assigned");
+        }
+    }
+
+
+    throw std::runtime_error("Unable to find variable: " + name);
+}
+
 Value Context::resolveVariable(std::string name) {
     Scope local;
     Scope global = this->m_globalScope;
 
     // Look through call stack for variable
-    for(int i = (int)this->m_callStack.size(); i--;) {
-        local = this->m_callStack[i];
+    for(int i = (int)this->m_scopeStack.size(); i--;) {
+        local = this->m_scopeStack[i];
 
         if(local.find(name) != local.end()) {
             return local[name];
